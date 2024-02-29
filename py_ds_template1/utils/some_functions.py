0a2febb980b33949c0310c83f1f5cd2275f3bcae
Eric Kelly <ericdatakelly@gmail.com>
2024-02-28 20:12:19 -0700

    Update isort config and run pre-commit

--- py_ds_template1/utils/some_functions.py
+++ py_ds_template1/utils/some_functions.py
@@ -1,5 +1,6 @@
 def add_one(x):
     return x + 1
 
+
 if __name__ == "__main__":
     print(add_one(1))
--- tests/test_some_functions.py
+++ tests/test_some_functions.py
@@ -1,5 +1,6 @@
 from py_ds_template1.utils.some_functions import add_one
 
+
 def test_add_one():
     assert add_one(1) == 2
     assert add_one(2) == 3
